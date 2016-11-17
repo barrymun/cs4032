51a6834aff331ba0c5e4bdfe7dace463fe4d04c4
elneelo <barrymun@tcd.ie>
2016-11-17 23:16:57 +0000

    chatroom class fix

---
+++ test/chatroom.py
@@ -0,0 +1,29 @@
+
+class ChatRoom:
+
+	def __init__(self, name, reference):
+		users = []
+		self.name = name
+		self.reference = reference
+
+	def add_user(username, socket):
+		if not socket in username:
+			users[socket] = username
+    socket.send("%s has joined this chatroom." %(username))
+
+  def remove_user(username, socket):
+    socket.send("%s has left this chatroom.", %(username))
+    users.delete(socket)
+    for i in users:
+    	if i == socket:
+    		users.pop(i)
+
+  def disconnect_user(socket):
+  	if socket in users:
+  		remove_user(users[socket], socket)
+
+  def send_message(message, from_socket):
+    username = users[from_socket]
+    for user in users:
+      msg = ("SEND: %s\nCLIENT_NAME: %s\nMESSAGE: %s\n\n" %(reference,username,message))
+      user.write(msg)
